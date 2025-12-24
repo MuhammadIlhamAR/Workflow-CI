a9f5834f4b49e2b016687471da408d18ba044e2e
MuhammadIlhamAR <m.ilham2408@gmail.com>
2025-12-25 02:21:43 +0700

    Update modelling.py

--- MLProject-Folder/modelling.py
+++ MLProject-Folder/modelling.py
@@ -3,40 +3,24 @@ import pandas as pd
 import numpy as np
 import mlflow
 import mlflow.sklearn
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
 def train_model(input_file, n_estimators, max_depth):
-    # 1. Load Data
+    mlflow.autolog(log_models=True)
+
+    # Load Data
     df = pd.read_csv(input_file)
     X = df.drop(['RAIN_Category'], axis=1)
     y = df['RAIN_Category'].map({'Tidak Hujan': 0, 'Hujan': 1})
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
-    # 2. MLflow Tracking (Manual Logging)
     with mlflow.start_run():
-        mlflow.log_param("n_estimators", n_estimators)
-        mlflow.log_param("max_depth", max_depth)
-
         model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, random_state=42)
         model.fit(X_train, y_train)
-
-        y_pred = model.predict(X_test)
-        acc = accuracy_score(y_test, y_pred)
-        mlflow.log_metric("accuracy", acc)
-
-        # Artefak Tambahan (Poin Advance)
-        report = classification_report(y_test, y_pred)
-        with open("classification_report.txt", "w") as f:
-            f.write(report)
-        mlflow.log_artifact("classification_report.txt")
-
-        mlflow.sklearn.log_model(model, "model")
-        print(f"✅ Training Selesai. Accuracy: {acc}")
+        
+        print(f"✅ Training Skilled Selesai. Cek MLflow UI/DagsHub untuk hasil autolog.")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
